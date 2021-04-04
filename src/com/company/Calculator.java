33d9b81be981e58b3e28054792d87ae5c8ad4194
Pashhaa <71789749+Pashhaa@users.noreply.github.com>
2021-04-04 13:50:28 +0300

    ClassAdding

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Calculator {
+    public static String ExpRPN(String Exp){
+        return null;
+    }
+
+    public static double RPNAnsw(String rpn){
+        return 0;
+    }
+
+    private int getP(char token){
+        if(token=='*'|| token =='/')return 3;
+        else if (token == '+' || token =='-' )return 2;
+        else if( token == '(')return 1;
+        else if (token == ')')return -1;
+        else return 0;
+    }
+}
